628b7d27e820d138117c57a4a25c700aec83961a
Thabani David Machila <133752978+bijila04@users.noreply.github.com>
2025-03-03 14:22:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import glob
 import pandas as pd
 import matplotlib.pyplot as plt 
 import matplotlib.ticker as mticker
-import seaborn as sns
 import plotly.express as px
 import streamlit as st
 from category_encoders import OneHotEncoder
